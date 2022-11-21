84e90752fb407ce9f44e1e2b8bb6cbb06ee3d14a
Horace Smith <horacesmith26@gmail.com>
2022-11-21 16:54:23 -0600

    functions exercises at codeup

---
+++ JS/functions-lessons.js
@@ -0,0 +1,13 @@
+let x = isEven (23456)
+console.log()
+fight("Billy","Bob")
+function myfunction(name) {
+    return "thank you" + ".you have ran my function";
+}
+function fight(person1,person2){
+    console.log(person1 + "punched " +person2 + " right in the kisser")
+}
+
+function isEven(num){
+return (num % 2==0)
+}
