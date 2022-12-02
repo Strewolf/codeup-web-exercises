613f121f35fa6a68427c0b578f8b23ef4dd6dd2c
Horace Smith <horacesmith26@gmail.com>
2022-12-01 21:59:55 -0600

    conditions exercise progress

---
+++ JS/break_and_continue.js
@@ -0,0 +1,14 @@
+while(true){
+    let userNum = prompt("ENTER AN ODD NUMBER BETWEEN 1 AND 50");
+    userNum= parseFloat(userNum);
+    if(userNum % 2==1 && userNum <= 50 && userNum >=1){
+    for(let i=1;1<=50;i+=2){
+        if(i === userNum){
+            continue;
+
+        }else{
+            console.log(`here is an odd number: ${i}`)
+        }
+        break;
+    }}
+}
--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,9 +1,3 @@
-var shapes = ['square', 'rectangle', 'circle', 'triangle'];
-
-// loop through the array and log the values
-for (var i = 0; i < shapes.length; i++) {
-    console.log('The shape at index ' + i + ' is: ' + shapes[i]);
-}
 
 
 
