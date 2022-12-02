ac74a643c6ee25967559c333eea0cd8553b36946
Horace Smith <horacesmith26@gmail.com>
2022-12-02 15:43:45 -0600

    array manipulation

--- JS/planets-array.js
+++ JS/planets-array.js
@@ -19,23 +19,31 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    planets.unshift('sun')
+    planets.unshift('Sun')
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto')
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift()
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop()
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+let index= planets.indexOf('Earth')
+    console.log(index)
 
     console.log("Reversing the order of the planets array.");
+planets.reverse()
     console.log(planets);
 
     console.log("Sorting the planets array.");
+
     console.log(planets);
+
 })();
