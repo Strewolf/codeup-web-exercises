adce9e16f6992dd8139d26aabd75514206d47d73
Horace Smith <horacesmith26@gmail.com>
2022-12-03 10:01:35 -0600

    array manipulation

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,4 +1,42 @@
 
 
 
+function showMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        let result= i * number
+        console.log (`${number} * ${i} = ${result}`)
+    }
+}
+ showMultiplicationTable(7)
+
+
+ let count=0
+ for (let i = 0; i < 10; i++){
+     for (let j=0; j<=Math.floor(Math.random()*(max - min));j++) count++;
+          if(count % 2 == 0){
+         console.log(`${count}  "is even"`)
+     } else{
+         console.log(`${count} "is odd"`)
+     }
+ }
+
+ function makePyramid(){
+     let row = 10
+     let output = ''
+     for(let i = 1; i<row;i++){
+         for (let j = 1; j<=i;j++){
+             output +=i;
+         }
+         console.log(output);output=''
+     }
+ }
+
+for(let i=100; i >= 0 ; i -=5){
+    console.log(i)
+}
+
+
+
+
+
 
