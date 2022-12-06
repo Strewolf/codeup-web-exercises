6e1d5cb31fd265be7ad73d41ffc64ab8c7711a84
Horace Smith <horacesmith26@gmail.com>
2022-12-05 18:27:41 -0600

    objects

--- JS/mini_exercise2.js
+++ JS/mini_exercise2.js
@@ -15,6 +15,9 @@ const users = [
 
 console.log(`${users[0].givenName} ${users[1].givenName} ${users[2].givenName}`)
 users.forEach(function(user){
+user.givenName= "John Doe"
+    console.log(user)
 
-}
+})
+
 
