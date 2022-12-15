7efce56975f7d4654efdbe041ca45f1f36fbd8f2
Horace Smith <horacesmith26@gmail.com>
2022-12-15 09:26:09 -0600

    box model

---
+++ js/returnpetswithnofish.js
@@ -0,0 +1,62 @@
+// Create a function, returnPetsWithNoFish, that takes in a array of pet objects and returns an array of pet objects with no pets of type 'Fish'.
+
+    const pets = [
+    {
+        name: 'Sparky',
+        type: 'Fish',
+        age: 4
+    },
+    {
+        name: 'Mr. Pig',
+        type: 'Cat',
+        age: 4
+    },
+    {
+        name: 'Bubba',
+        type: 'Dog',
+        age: 5
+    },
+    {
+        name: 'Beans',
+        type: 'Dog',
+        age: 3
+    },
+    {
+        name: 'Mr. Salmon',
+        type: 'Fish',
+        age: 1
+    }
+];
+
+
+
+
+
+// returnPetsWithNoFish(pets) // returns...
+//
+//     [
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//         {
+//             name: 'Bubba',
+//             type: 'Dog',
+//             age: 5
+//         },
+//         {
+//             name: 'Beans',
+//             type: 'Dog',
+//             age: 3
+//         }
+//     ]
+
+
+
+
+
+
+
+
+
