67b9843bb334664f53c989443e2824a46a1ab476
Horace Smith <horacesmith26@gmail.com>
2022-12-16 09:37:58 -0600

    box model

--- js/longestname.js
+++ js/longestname.js
@@ -1,5 +1,49 @@
-var longest = arr.reduce(
-    function (a, b) {
-        return a.length > b.length ? a : b;
+
+// Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
+
+    const pets = [
+    {
+        name: 'Sparky',
+        type: 'Fish',
+        age: 4
+    },
+    {
+        name: 'Mr. Pig',
+        type: 'Cat',
+        age: 4
+    },
+    {
+        name: 'Bubba',
+        type: 'Dog',
+        age: 5
+    },
+    {
+        name: 'Beans',
+        type: 'Dog',
+        age: 3
+    },
+    {
+        name: 'Mr. Salmon',
+        type: 'Fish',
+        age: 1
     }
-);
+];
+
+returnLongestPetName(pets) // returns 'Mr. Salmon'
+
+function returnLongestPetName(pets) {
+    let longestName = '';
+    let longestNameLength = 0;
+
+    pets.forEach(pet => {
+        if (pet.name.length > longestNameLength) {
+            longestName = pet.name;
+            longestNameLength = pet.name.length;
+        }
+    });
+
+    return longestName;
+}
+
+const longestName=returnLongestPetName(pets);
+console.log(longestName)
