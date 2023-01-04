f23eba325b6cf7cb1985899e7a9558da0e19dfe0
Horace Smith <horacesmith26@gmail.com>
2023-01-04 09:24:18 -0600

    bootstrap

---
+++ js/jan,4 exercise.js
@@ -0,0 +1,11 @@
+// Create a function, filterList, that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// filterList([1,2,‘a’,‘b’]) // returns [1,2]
+// filterList([1,‘a’,‘b’,0,15]) // returns [1,0,15]
+// filterList([1,2,‘aasf’,‘1’,‘123’,123]) // returns [1,2,123]
+
+function filterList(list){
+    return list.filter(function(item){
+        return typeof item==="number"
+    })
+
+}
