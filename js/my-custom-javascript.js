60a731a5f346318d0b3d2df96181f2fb479e267a
Horace Smith <horacesmith26@gmail.com>
2023-01-08 15:56:20 -0600

    jquery exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,10 @@
 "use strict";
 
-$(function() {
-    alert( 'The DOM has finished loading!' );
+$(".codeup").css("border","1px solid red")
+
+$("li").css("font-size","20px")
+
+
+$(function (){
+    alert($("#code").text());
 });
-
-
-
