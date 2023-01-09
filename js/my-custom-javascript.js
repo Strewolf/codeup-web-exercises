73d9d87bbc597a43fbfa42c958fcb2e26bd391ab
Horace Smith <horacesmith26@gmail.com>
2023-01-08 20:04:40 -0600

    jquery exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,17 +1,38 @@
 "use strict";
 
-$(".codeup").css("border","1px solid red")
-
-$("li").css("font-size","20px")
-
-
-$(function (){
-    alert($("#code").text());
-});
-
-$(function (){
-    alert($("h1").text());
-});
+//
 $(document).ready(function() {
-    $('h1, p, li').css('background-color', 'yellow');
+    $('h1').click(function() {
+        if ($(this).css('background-color') == 'rgb(255, 255, 0)') {
+            $(this).css('background-color', 'transparent');
+        } else {
+            $(this).css('background-color', 'yellow');
+        }
+    });
 });
+
+
+// $(document).ready(function() {
+//     $('p').dblclick(function() {
+//         $(this).css('font-size', '18px');
+//     });
+// });
+$(document).ready(function() {
+    $('p').dblclick(function() {
+        if ($(this).css('font-size') == '18px') {
+            $(this).css('font-size', '');
+        } else {
+            $(this).css('font-size', '18px');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('li').mouseenter(function() {
+        $(this).css('color', 'red');
+    });
+    $('li').mouseleave(function() {
+        $(this).css('color', 'black');
+    });
+});
+
