f55ee58579269a99dbcf0db9c55b7feb596da73f
Horace Smith <horacesmith26@gmail.com>
2023-01-08 19:17:01 -0600

    jquery exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -8,3 +8,10 @@ $("li").css("font-size","20px")
 $(function (){
     alert($("#code").text());
 });
+
+$(function (){
+    alert($("h1").text());
+});
+$(document).ready(function() {
+    $('h1, p, li').css('background-color', 'yellow');
+});
