30959e37cddf3a8a68e29bef71a17939306dcb87
Horace Smith <horacesmith26@gmail.com>
2023-01-23 14:31:01 -0600

    weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,3 +1,5 @@
+
+
 // / const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast";
 // const OPEN_WEATHER_APPID= 24c04769a5bbc5ce9b6861e15c1f7494
 // $.get(API_ENDPOINT, {
@@ -28,21 +30,37 @@
 
 let button=document.querySelector(".button")
 let inputValue=document.querySelector(".inputValue")
-let name=document.querySelector(".name")
-let desc=document.querySelector(".desc")
-let temp=document.querySelector(".temp")
+let display=document.querySelector(".display")
 
-    // button.addEventListener("click",function (){
-    //     $.ajax("https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=24c04769a5bbc5ce9b6861e15c1f7494")
-    //         .then(response => response.json())
-    //         .then(data =>console.log(data))
-    //
-    //         .catch(err => alert("wrong city name!"))
-    // })
-    .button.addEventListener("click",function (){
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"weatherKey")
-            .then(response => response.json())
-            .then(data =>console.log(data))
-            .catch(err => alert("wrong city name!"))
-    })
+button.addEventListener("click", function (){
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"&Appid="+weatherKey+"&units=imperial")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let card;
+            let name;
+            let desc;
+            let temp;
+            for(let i = 0; i < 5; i++) {
+                let card = document.createElement("div");
+                card.classList.add("card");
+                card = document.createElement("card");
+                card.classList.add("day"+(i+1));
+                name = document.createElement("h1");
+                name.classList.add("name"+(i+1));
+                name.innerHTML = data.city.name + ', ' + data.city.country;;
+                desc = document.createElement("p");
+                desc.classList.add("desc"+(i+1));
+                desc.innerHTML = data.list[i].weather[0].description;
+                temp = document.createElement("p");
+                temp.classList.add("temp"+(i+1));
+                temp.innerHTML = data.list[i*8].main.temp + "°F";
+                card.appendChild(name);
+                card.appendChild(desc);
+                card.appendChild(temp);
+                display.appendChild(card);
+            }
+        })
+        .catch(err => alert("Wrong city name!"))
+});
 
