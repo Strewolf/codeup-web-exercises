82ba777387c2faddad1208399399e2186c4099ec
Horace Smith <horacesmith26@gmail.com>
2023-01-27 23:59:02 -0600

    weather map

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,26 +35,26 @@ const users = [
         yearsOfExperience: 9
     }
 ];
-// const threeLanguages = users.filter(user => user.languages.length >= 3);
-// console.log(threeLanguages)
-//
-//
-// const emailAddress = users.map(user=>user.email)
-// console.log(emailAddress)
-//
-//
-// const totalYears = users.yearsOfExperience.reduce((accumulator, currentValue) => {
-//     return accumulator + currentValue;
-// }, 0);
-// const averageYears=totalYears/user.length;
-// console.log(averageYears)
-//
-//
-// const longestEmail = users.email.reduce((accumulator, currentValue) => {
-//     return accumulator.length > currentValue.length ? accumulator : currentValue;
-// }, '');
-// console.log(longestEmail)
-//
+const threeLanguages = users.filter(user => user.languages.length >= 3);
+console.log(threeLanguages)
+
+
+const emailAddress = users.map(user=>user.email)
+console.log(emailAddress)
+
+
+const totalYears = users.yearsOfExperience.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+const averageYears=totalYears/user.length;
+console.log(averageYears)
+
+
+const longestEmail = users.email.reduce((accumulator, currentValue) => {
+    return accumulator.length > currentValue.length ? accumulator : currentValue;
+}, '');
+console.log(longestEmail)
+
 
 const names = users.reduce((accumulator, currentValue) => {
     return accumulator + ', ' + currentValue.name;
@@ -62,11 +62,11 @@ const names = users.reduce((accumulator, currentValue) => {
 console.log(names);
 
 
-// const languages = users.reduce((accumulator, currentValue) => {
-//     return accumulator.concat(currentValue.languages);
-// }, []);
-// console.log(languages); // ['clojure', 'javascript', 'java', 'scala', 'php', 'javascript', 'bash', 'java', 'php', 'sql', 'html', 'css', 'javascript', 'php']
-// const uniqueLanguages = [...new Set(languages)];
-// console.log(uniqueLanguages); // ['clojure', 'javascript', 'java', 'scala', 'php', 'bash', 'sql', 'html', 'css']
-//
+const languages = users.reduce((accumulator, currentValue) => {
+    return accumulator.concat(currentValue.languages);
+}, []);
+console.log(languages); // ['clojure', 'javascript', 'java', 'scala', 'php', 'javascript', 'bash', 'java', 'php', 'sql', 'html', 'css', 'javascript', 'php']
+const uniqueLanguages = [...new Set(languages)];
+console.log(uniqueLanguages); // ['clojure', 'javascript', 'java', 'scala', 'php', 'bash', 'sql', 'html', 'css']
+
 
