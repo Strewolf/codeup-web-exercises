c642b6c2fa6d8e0af9bdb312675a0fc8f3ba728a
Horace Smith <horacesmith26@gmail.com>
2023-01-29 21:51:10 -0600

    weather map

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,28 @@
 
 
-function getLastCommitDate(username) {
-    return fetch(`https://api.github.com/users/${username}/events`{headers: {}})
-        .then(response => response.json())
-        .then(events => events.filter(event => event.type === "PushEvent"))
-        .then(pushEvents => pushEvents[0].created_at)
+async function getLastCommit(username) {
+    const url = `https://api.github.com/users/${username}/events`;
+
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `GithubT`
+        }
+    });
+
+    const data = await response.json();
+
+    const lastCommit = data.filter(event => event.type === 'PushEvent')[0];
+
+    const date = new Date(lastCommit.created_at);
+    const options = { timeZone: 'America/Chicago' };
+    return date.toLocaleString('default', options);
 }
+
+async function testGetLastCommit() {
+    const username = 'Strewolf';
+    const lastCommitDate = await getLastCommit(username);
+    console.log(lastCommitDate);
+}
+
+testGetLastCommit();
+
